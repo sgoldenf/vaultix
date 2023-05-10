34c4d75605c1bcbb1981e8ffc2eab060390a1974
Shawnna Goldenfold <sgoldenf@student.21-school.ru>
2023-05-10 18:46:53 +0700

    feat: added base64 encrypting in GenerateKey && added DecryptPassword

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -4,15 +4,17 @@ import (
 	"crypto/aes"
 	"crypto/cipher"
 	"crypto/rand"
+	"encoding/base64"
 	"io"
 )
 
-func GenerateKey() ([]byte, error) {
+func GenerateKey() (string, error) {
 	key := make([]byte, 16)
 	if _, err := rand.Read(key); err != nil {
-		return nil, err
+		return "", err
 	}
-	return key, nil
+	encodedPassword := base64.StdEncoding.EncodeToString([]byte(key))
+	return encodedPassword, nil
 }
 
 func EncryptPassword(password, key []byte) ([]byte, error) {
@@ -28,4 +30,20 @@ func EncryptPassword(password, key []byte) ([]byte, error) {
 	stream := cipher.NewCFBEncrypter(cipherBlock, initVector)
 	stream.XORKeyStream(ciphertext[aes.BlockSize:], password)
 	return ciphertext, nil
+}
+
+func DecryptPassword(encryptedPassword, key []byte) (string, error) {
+	cipherBytes, err := base64.URLEncoding.DecodeString(string(encryptedPassword))
+	if err != nil {
+		return "", nil
+	}
+	initVector := cipherBytes[:aes.BlockSize]
+	cipherBytes = cipherBytes[aes.BlockSize:]
+	cipherBlock, err := aes.NewCipher(key)
+	if err != nil {
+		return "", err
+	}
+	stream := cipher.NewCFBDecrypter(cipherBlock, initVector)
+	stream.XORKeyStream(cipherBytes, cipherBytes)
+	return string(cipherBytes), nil
 }
