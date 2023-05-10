a5f66c20b10234c8bf6a27268f4c50d03c852def
sgoldenf <sgoldenf@student.21-school.ru>
2023-05-11 01:30:00 +0700

    fix: fixed password encryption

--- internal/models/errors.go
+++ internal/models/errors.go
@@ -6,5 +6,4 @@ var (
 	ErrNoRecord              = errors.New("no matching record found")
 	ErrInvalidMasterPassword = errors.New("invalid master password")
 	ErrDuplicateID           = errors.New("user with this telegram id already exists")
-	ErrDuplicateCredentials  = errors.New("there already is a password for this service-login pair")
 )
--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -5,45 +5,67 @@ import (
 	"crypto/cipher"
 	"crypto/rand"
 	"encoding/base64"
+	"errors"
 	"io"
 )
+
+const keySize = 32
 
 func GenerateKey() (string, error) {
 	key := make([]byte, 16)
 	if _, err := rand.Read(key); err != nil {
 		return "", err
 	}
-	encodedPassword := base64.StdEncoding.EncodeToString([]byte(key))
+	encodedPassword := base64.StdEncoding.EncodeToString(key)
 	return encodedPassword, nil
 }
 
-func EncryptPassword(password, key []byte) ([]byte, error) {
-	cipherBlock, err := aes.NewCipher(key)
-	if err != nil {
-		return nil, err
-	}
-	ciphertext := make([]byte, aes.BlockSize+len(password))
-	initVector := ciphertext[:aes.BlockSize]
-	if _, err := io.ReadFull(rand.Reader, initVector); err != nil {
-		return nil, err
-	}
-	stream := cipher.NewCFBEncrypter(cipherBlock, initVector)
-	stream.XORKeyStream(ciphertext[aes.BlockSize:], password)
-	return ciphertext, nil
+func Encode(b []byte) string {
+	return base64.StdEncoding.EncodeToString(b)
 }
 
-func DecryptPassword(encryptedPassword, key []byte) (string, error) {
-	cipherBytes, err := base64.URLEncoding.DecodeString(string(encryptedPassword))
+func Decode(s string) []byte {
+	data, err := base64.StdEncoding.DecodeString(s)
 	if err != nil {
-		return "", nil
+		panic(err)
 	}
-	initVector := cipherBytes[:aes.BlockSize]
-	cipherBytes = cipherBytes[aes.BlockSize:]
+	return data
+}
+
+func EncryptPassword(password, masterPassword string) (string, error) {
+	key := []byte(masterPassword)[:keySize]
 	cipherBlock, err := aes.NewCipher(key)
 	if err != nil {
 		return "", err
 	}
-	stream := cipher.NewCFBDecrypter(cipherBlock, initVector)
-	stream.XORKeyStream(cipherBytes, cipherBytes)
-	return string(cipherBytes), nil
+	initVector := make([]byte, aes.BlockSize)
+	if _, err := io.ReadFull(rand.Reader, initVector); err != nil {
+		return "", err
+	}
+	passwordBytes := []byte(password)
+	cfb := cipher.NewCFBEncrypter(cipherBlock, initVector)
+	cipherPassword := make([]byte, len(passwordBytes))
+	cfb.XORKeyStream(cipherPassword, passwordBytes)
+	encodedPassword := make([]byte, aes.BlockSize+len(cipherPassword))
+	copy(encodedPassword, initVector)
+	copy(encodedPassword[aes.BlockSize:], cipherPassword)
+	return Encode(encodedPassword), nil
+}
+
+func DecryptPassword(encryptedPassword, masterPassword string) (string, error) {
+	key := []byte(masterPassword)[:keySize]
+	cipherBlock, err := aes.NewCipher(key)
+	if err != nil {
+		return "", nil
+	}
+	cipherPassword := Decode(encryptedPassword)
+	if len(cipherPassword) < aes.BlockSize {
+		return "", errors.New("ciphertext too short")
+	}
+	initVector := cipherPassword[:aes.BlockSize]
+	cipherPassword = cipherPassword[aes.BlockSize:]
+	cfb := cipher.NewCFBDecrypter(cipherBlock, initVector)
+	passwordBytes := make([]byte, len(cipherPassword))
+	cfb.XORKeyStream(passwordBytes, cipherPassword)
+	return string(passwordBytes), nil
 }
