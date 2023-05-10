70066c4ce045e0f1f5c02c8c9622bf437fea83fd
Shawnna Goldenfold <sgoldenf@student.21-school.ru>
2023-05-10 18:47:16 +0700

    fix: fixed sql queries

--- migrations/init_db.sql
+++ migrations/init_db.sql
@@ -1,7 +1,8 @@
 create table if not exists users (
-    id bigint not null primary key,
-    hashed_password char(60) not null,
+    id bigint primary key,
+    hashed_password char(60) not null
 );
+
 create table if not exists passwords (
     id serial primary key,
     user_id bigint not null,
@@ -9,6 +10,7 @@ create table if not exists passwords (
     login varchar(255) not null,
     encrypted_password bytea not null,
     constraint fk_user foreign key(user_id) references users(id),
-    constraint passwords_user_service_login_uc unique (user_id, service, login),
-    index user_id_idx (user_id)
+    constraint passwords_user_service_login_uc unique (user_id, service, login)
 );
+
+create index if not exists user_id_idx on passwords (user_id);
