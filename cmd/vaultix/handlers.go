6305f27ca088cff0f70e5b0082222aef5e3d2553
Shawnna Goldenfold <sgoldenf@student.21-school.ru>
2023-05-10 18:43:11 +0700

    fix: escaping special symbols in messages && notifying about successful handleSet

--- cmd/vaultix/handlers.go
+++ cmd/vaultix/handlers.go
@@ -35,8 +35,8 @@ func (app *application) handleStart(message *tgbotapi.Message) error {
 func (app *application) handleExistingUser(userID int64) {
 	msg := tgbotapi.NewMessage(userID,
 		`User with your Telegram ID already exists.
-		If you forgot your Master Password and want to get a new one, press Restart Button.
-		WARNING: if you proceed, all data for your account will be deleted`)
+If you forgot your Master Password and want to get a new one, press Restart Button.
+WARNING: if you proceed, all data for your account will be deleted`)
 	msg.ReplyMarkup = restartKeyboard
 	if _, err := app.bot.Send(msg); err != nil {
 		app.errorLog.Println(err)
@@ -47,56 +47,64 @@ func (app *application) handleSet(message *tgbotapi.Message) error {
 	args := strings.Split(message.CommandArguments(), " ")
 	if len(args) != 4 {
 		app.notifyUser(message.From.ID,
-			`Usage: "/set <service> <login> <password> <Master Password>"`,
-		)
+			`Usage: "/set <service> <login> <password> <Master Password>"`, "")
 		return nil
 	}
 	if _, err := app.userModel.Authenticate(message.From.ID, args[3]); err != nil {
 		if err == models.ErrInvalidMasterPassword {
 			app.notifyUser(message.From.ID,
-				`Invalid Master Password`,
-			)
+				`Invalid Master Password`, "")
 			return nil
 		} else {
 			return err
 		}
 	}
 	if err := app.passwordModel.AddPassword(
-		message.From.ID, args[0], args[1], args[2], args[3]); err != nil {
+		message.From.ID, args[0], args[1], args[2], args[3],
+	); err != nil {
 		if err == models.ErrDuplicateCredentials {
 			app.notifyUser(message.From.ID,
 				`There already is a password for this service-login pair.
-				You can use /del to delete it first.`,
-			)
+You can use /del to delete it first.`, "")
+			return nil
 		} else {
-			app.notifyUser(message.From.ID,
-				`Your credentials to this has been successfuly added to password manager.
-				For security purposes, we recommend you to delete your messages containing any credentials.`,
-			)
 			return err
 		}
 	}
+	app.notifyUser(message.From.ID,
+		`Your credentials to this service has been successfuly added to password manager.
+For security purposes, we recommend you to delete your messages containing any credentials.`, "")
 	return nil
 }
 
 func (app *application) handleGet(message *tgbotapi.Message) error {
 	args := strings.Split(message.CommandArguments(), " ")
 	if len(args) != 2 {
-		app.notifyUser(message.From.ID,
-			`Usage: "/get <service> <Master Password>"`,
-		)
+		app.notifyUser(message.From.ID, `Usage: "/get <service> <Master Password>"`, "")
 		return nil
 	}
 	if _, err := app.userModel.Authenticate(message.From.ID, args[1]); err != nil {
 		if err == models.ErrInvalidMasterPassword {
-			app.notifyUser(message.From.ID,
-				`Invalid Master Password`,
-			)
+			app.notifyUser(message.From.ID, `Invalid Master Password`, "")
 			return nil
 		} else {
 			return err
 		}
 	}
+	passwords, err := app.passwordModel.GetPasswords(message.From.ID, args[0], args[1])
+	if err != nil {
+		return err
+	}
+	if len(passwords) == 0 {
+		app.notifyUser(message.From.ID,
+			"You don't have credentials for "+args[0]+" service", "")
+		return nil
+	}
+	text := "Your credentials for " + args[0] + " service:"
+	for _, password := range passwords {
+		text += fmt.Sprintf("\n\nLogin: `%s`\nPassword: `%s`", password.Login, password.Password)
+	}
+	app.notifyUser(message.From.ID, text, tgbotapi.ModeMarkdownV2)
 	return nil
 }
 
@@ -112,13 +120,17 @@ func (app *application) handleDefault(message *tgbotapi.Message) error {
 	return nil
 }
 
-func (app *application) notifyUser(userID int64, text string) {
-	errorMsg := tgbotapi.NewMessage(userID, "")
-	if errorMsg.Text = text; errorMsg.Text == "" {
-		errorMsg.ParseMode = "MarkdownV2"
-		errorMsg.Text = "Oops, something went wrong. Please try again later or contact @sgoldenf for technical support"
-	}
-	if _, err := app.bot.Send(errorMsg); err != nil {
+func (app *application) errorNotification(userID int64) {
+	app.notifyUser(userID,
+		`Oops, something went wrong\. Please try again later or contact @sgoldenf for technical support\.`,
+		tgbotapi.ModeMarkdownV2,
+	)
+}
+
+func (app *application) notifyUser(userID int64, text string, parseMode string) {
+	msg := tgbotapi.NewMessage(userID, text)
+	msg.ParseMode = parseMode
+	if _, err := app.bot.Send(msg); err != nil {
 		app.errorLog.Println(err)
 	}
 }
@@ -128,16 +140,14 @@ func (app *application) createUser(userID int64) error {
 	if err != nil {
 		return err
 	}
-	msg := tgbotapi.NewMessage(userID, "")
-	msg.ParseMode = "markdown"
-	msg.Text = `Here is your Master Password: ` + "`" + masterPassword + "`" + `
-	You'll need it to have access to saved passwords.
-	Store it in a safe place and then you can delete this message.
-	WARNING: If you'll loose/forget your password, you won't have access to your data.
-	You'll be able to restart bot using /start command and get new Master Password, but all previous data will be lost.`
-	if _, err := app.bot.Send(msg); err != nil {
-		return err
-	}
+	app.notifyUser(userID,
+		`Here is your Master Password: `+"`"+masterPassword+"`"+`
+You'll need it to have access to saved passwords\.
+Store it in a safe place and then you can delete this message\.
+WARNING: If you'll loose/forget your password, you won't have access to your data\.
+You'll be able to restart bot using /start command and get new Master Password, but all previous data will be lost\.`,
+		tgbotapi.ModeMarkdownV2,
+	)
 	return nil
 }
 
@@ -146,12 +156,7 @@ func (app *application) deleteUser(UserID int64) error {
 	if err != nil {
 		return err
 	}
-	msg := tgbotapi.NewMessage(
-		UserID,
-		fmt.Sprint("Deleted ", delPasswords, " from ", delUsers, " user(s)."),
-	)
-	if _, err := app.bot.Send(msg); err != nil {
-		app.errorLog.Println(err)
-	}
+	app.notifyUser(UserID,
+		fmt.Sprint("Deleted ", delPasswords, " passwords from ", delUsers, " user(s)."), "")
 	return nil
 }
