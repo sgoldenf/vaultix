6d20f1e86b299d45ca479e2600556d1a9185a151
sgoldenf <sgoldenf@student.21-school.ru>
2023-05-11 01:26:32 +0700

    feat: handling get, del, help commands && default handler

--- cmd/vaultix/handlers.go
+++ cmd/vaultix/handlers.go
@@ -44,6 +44,7 @@ WARNING: if you proceed, all data for your account will be deleted`)
 }
 
 func (app *application) handleSet(message *tgbotapi.Message) error {
+	defer app.deleteMessage(message.Chat.ID, message.MessageID)
 	args := strings.Split(message.CommandArguments(), " ")
 	if len(args) != 4 {
 		app.notifyUser(message.From.ID,
@@ -59,25 +60,26 @@ func (app *application) handleSet(message *tgbotapi.Message) error {
 			return err
 		}
 	}
+	if exists, err := app.passwordModel.Exists(message.From.ID, args[0], args[1]); err != nil {
+		return err
+	} else if exists {
+		app.notifyUser(message.From.ID,
+			`There already is a password for this service-login pair.
+You can use /del to delete it first.`, "")
+		return nil
+	}
 	if err := app.passwordModel.AddPassword(
 		message.From.ID, args[0], args[1], args[2], args[3],
 	); err != nil {
-		if err == models.ErrDuplicateCredentials {
-			app.notifyUser(message.From.ID,
-				`There already is a password for this service-login pair.
-You can use /del to delete it first.`, "")
-			return nil
-		} else {
-			return err
-		}
+		return err
 	}
 	app.notifyUser(message.From.ID,
-		`Your credentials to this service has been successfuly added to password manager.
-For security purposes, we recommend you to delete your messages containing any credentials.`, "")
+		`Your credentials to this service has been successfuly added to password manager.`, "")
 	return nil
 }
 
 func (app *application) handleGet(message *tgbotapi.Message) error {
+	defer app.deleteMessage(message.Chat.ID, message.MessageID)
 	args := strings.Split(message.CommandArguments(), " ")
 	if len(args) != 2 {
 		app.notifyUser(message.From.ID, `Usage: "/get <service> <Master Password>"`, "")
@@ -109,54 +111,42 @@ func (app *application) handleGet(message *tgbotapi.Message) error {
 }
 
 func (app *application) handleDel(message *tgbotapi.Message) error {
+	args := strings.Split(message.CommandArguments(), " ")
+	if len(args) != 1 {
+		app.notifyUser(message.From.ID,
+			`Usage: "/del <service>"`, "",
+		)
+		return nil
+	}
+	if deleted, err := app.passwordModel.DeletePasswords(message.From.ID, args[0]); err != nil {
+		return err
+	} else if deleted == 0 {
+		app.notifyUser(message.From.ID,
+			`You don't have credentials for `+args[0]+` service.`, "",
+		)
+	} else {
+		app.notifyUser(message.From.ID,
+			fmt.Sprint(deleted, ` passwords deleted from `, args[0], ` service.`), "",
+		)
+	}
 	return nil
 }
 
 func (app *application) handleHelp(message *tgbotapi.Message) error {
+	app.notifyUser(message.From.ID,
+		`Usage:
+/start - init using vaultix and get Master Password
+/set <service> <login> <password> <Master Password> - add login and password to service
+/get <service> <Master Password> - retrieve login and password by service name
+/del <service> <login> - delete login and password for service
+/help all available commands`, "",
+	)
 	return nil
 }
 
 func (app *application) handleDefault(message *tgbotapi.Message) error {
-	return nil
-}
-
-func (app *application) errorNotification(userID int64) {
-	app.notifyUser(userID,
-		`Oops, something went wrong\. Please try again later or contact @sgoldenf for technical support\.`,
-		tgbotapi.ModeMarkdownV2,
+	app.notifyUser(message.From.ID,
+		`I don't know `+message.Command()+` command :(`, "",
 	)
-}
-
-func (app *application) notifyUser(userID int64, text string, parseMode string) {
-	msg := tgbotapi.NewMessage(userID, text)
-	msg.ParseMode = parseMode
-	if _, err := app.bot.Send(msg); err != nil {
-		app.errorLog.Println(err)
-	}
-}
-
-func (app *application) createUser(userID int64) error {
-	masterPassword, err := app.userModel.CreateUser(userID)
-	if err != nil {
-		return err
-	}
-	app.notifyUser(userID,
-		`Here is your Master Password: `+"`"+masterPassword+"`"+`
-You'll need it to have access to saved passwords\.
-Store it in a safe place and then you can delete this message\.
-WARNING: If you'll loose/forget your password, you won't have access to your data\.
-You'll be able to restart bot using /start command and get new Master Password, but all previous data will be lost\.`,
-		tgbotapi.ModeMarkdownV2,
-	)
-	return nil
-}
-
-func (app *application) deleteUser(UserID int64) error {
-	delPasswords, delUsers, err := app.userModel.DeleteUser(UserID)
-	if err != nil {
-		return err
-	}
-	app.notifyUser(UserID,
-		fmt.Sprint("Deleted ", delPasswords, " passwords from ", delUsers, " user(s)."), "")
 	return nil
 }
